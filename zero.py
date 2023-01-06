4eec0237a43b391911f8770092bcd29aa16b433c
helenoninjaki <122110143+helenoninjaki@users.noreply.github.com>
2023-01-06 21:01:15 +0200

    Update zero.py

--- zero.py
+++ zero.py
@@ -1,5 +1,19 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)
+
+
 import numpy as np
 S=3
 A=4
 D = np.zeros([S, A])
 print(D)
+
+@app.route("/")
+def index():
+    
+    return render_template("index.html", url=url)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5300)))
